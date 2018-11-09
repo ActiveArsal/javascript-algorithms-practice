1efe927df014213fafa683e4d2e6b52d92b3d97d
Arsal Mehran <arsalmehran@gmail.com>
2018-11-09 06:17:01 -0800

    added tree.js and levelWidth.js

---
+++ levelWidth.js
@@ -0,0 +1,27 @@
+// --- Directions
+// Given the root node of a tree, return
+// an array where each element is the width
+// of the tree at each level.
+// --- Example
+// Given:
+//      0
+//    / |  \
+//  1   2    3
+//  |        |
+//  4        5
+// Answer: [1, 3, 2]
+
+function levelWidth(root) {
+	let arr = [root];
+	const counters = [];
+
+	while(arr.length) {
+		counters.push(arr.length);
+		arr.forEach(node => {
+			arr.push(...node.children);
+		})
+		arr.slice(counters[counters.length -1]);
+	}
+
+	return counters;
+}
